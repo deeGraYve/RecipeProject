d1daf76e37b74b493e35f66aaa812f2685229ae7
Erin Hamalainen <erin.v.hamalainen@gmail.com>
2013-08-29 18:43:19 -0400

    implemented update method

--- src/com/recipeproject/RecipeProvider.java
+++ src/com/recipeproject/RecipeProvider.java
@@ -166,10 +166,28 @@ public class RecipeProvider extends ContentProvider {
 }
 
 	@Override
-	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		int count;
+		switch(sUriMatcher.match(uri)) {
+		case INCOMING_RECIPE_COLLECTION_URI_INDICATOR:
+			count = db.update(RecipeTableMetaData.TABLE_NAME, values, where, whereArgs);
+			break;
+		
+		case INCOMING_SINGLE_RECIPE_URI_INDICATOR:
+			String rowId = uri.getPathSegments().get(1);
+			count = db.update(RecipeTableMetaData.TABLE_NAME, 
+					values, 
+					RecipeTableMetaData._ID + "=" + rowId + (!TextUtils.isEmpty(where) ? "AND (" + where + ")" : ""), 
+					whereArgs);
+			break;
+			
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);		
+		}
+		
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
 	}
 
 }
